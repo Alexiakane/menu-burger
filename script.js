d5ebd2b3d3a8c86a29a75246ac00e2fb5c30fd3c
Alexiakane <magdalena.cache@hotmail.com>
2024-12-16 23:17:47 +0100

    maj

--- script.js
+++ script.js
@@ -5,4 +5,26 @@ function myFunction() {
     } else {
       x.className = "topnav";
     }
+    let burger = document.getElementById("burger");
+    let close = document.getElementById("close");
+  /*  if (burger.style.display === "block" || burger.style.display === ""){ 
+      burger.style.display = "none";
+      close.style.display = "block";
+    } else {
+      burger.style.display = "block";
+      close.style.display = "none";
+    }*/
+    /*  if (burger.style.display === "none"){ 
+        burger.style.display = "block";
+        close.style.display = "none";
+      } else {
+        burger.style.display = "none";
+        close.style.display = "block";
+      }*/
+      if(burger.getAttribute("src")=== "menuB.PNG"){
+        burger.setAttribute("src","fermer.PNG");
+      } else {
+        burger.setAttribute("src", "menuB.PNG");
+      }
+
 }
